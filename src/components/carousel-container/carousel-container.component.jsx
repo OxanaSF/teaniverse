2fbb39d303290acd987cccb5e23857b01dc4ec0b
OxanaSF <oxanatango@gmail.com>
2022-12-03 21:00:46 -0800

    finish hero section

--- src/components/carousel-container/carousel-container.component.jsx
+++ src/components/carousel-container/carousel-container.component.jsx
@@ -5,15 +5,26 @@ import './carousel-container.styles.scss';
 
 const CarouselContainer = () => {
   const slides = [
-    // `${process.env.PUBLIC_URL}/images/slides-main-bg/gifts_bg.webp`,
-    // `${process.env.PUBLIC_URL}/images/slides-main-bg/subscribe-bg.webp`,
-    // `${process.env.PUBLIC_URL}/images/slides-main-bg/tea-main-bg.webp`,
-    // `${process.env.PUBLIC_URL}/images/slides-main-bg/delivery.webp`,
-
-    `https://www.chay.info/upload/webp/iblock/c4b/05m4ie3yfwm3wmkb0ocpsx04wdr4szao.webp`,
-    `//www.chay.info/upload/webp/iblock/4fe/1dan862gx22ugkj8969wmufzp8xx3ui1.webp`,
-    `https://www.chay.info/upload/iblock/59b/59b3d5e272287f94fcea8607bf33b56b.webp`,
-    `https://www.chay.info/upload/webp/iblock/e12/me0ie611hthyay9rv9up38nulltv0gip.webp`,
+    {
+      url: `https://www.chay.info/upload/webp/iblock/c4b/05m4ie3yfwm3wmkb0ocpsx04wdr4szao.webp`,
+      heroHeader: 'Christmas gifts',
+      heroText: 'Colleagues, friends, relatives',
+    },
+    {
+      url: `//www.chay.info/upload/webp/iblock/4fe/1dan862gx22ugkj8969wmufzp8xx3ui1.webp`,
+      heroHeader: 'Home Delivery subscription',
+      heroText: 'Get quality product fast and easy',
+    },
+    {
+      url: `https://www.chay.info/upload/iblock/59b/59b3d5e272287f94fcea8607bf33b56b.webp`,
+      heroHeader: 'Tea Deals',
+      heroText: 'Аor large volumes',
+    },
+    {
+      url: `https://www.chay.info/upload/webp/iblock/e12/me0ie611hthyay9rv9up38nulltv0gip.webp`,
+      heroHeader: 'International delivery',
+      heroText: 'Us a consultant for details',
+    },
   ];
 
   return (
@@ -26,10 +37,14 @@ const CarouselContainer = () => {
           animationHandler="fade"
           showArrows={false}
           showStatus={false}
-          interval={5000}
+          interval={4000}
         >
-          {slides.map((img, index) => (
-            <img key={index} className="carousel-img" src={img} alt="tea" />
+          {slides.map((slide, index) => (
+            <div className="carousel-img">
+              <img key={index} src={slide.url} alt="tea" />
+              <h1>{slide.heroHeader}</h1>
+              <p>{slide.heroText}</p>
+            </div>
           ))}
         </Carousel>
       </div>
