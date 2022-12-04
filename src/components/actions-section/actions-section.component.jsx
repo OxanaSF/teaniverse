a5550b242adccf20d0b3f9514cdc65f5874eb58d
OxanaSF <oxanatango@gmail.com>
2022-12-04 00:49:44 -0800

    create the bonus card section

---
+++ src/components/actions-section/actions-section.component.jsx
@@ -0,0 +1,33 @@
+import Button from '../../components/button/button.component';
+
+import './actions-section.styles.scss';
+
+const ActionsSection = () => {
+  return (
+    <section className="action-section">
+      <div className="action-section-text">
+        <h2>Bonus card</h2>
+        <p>Get discounts, bonuses and more with the Teaniverse Bonus Card</p>
+      </div>
+      
+      <div className="cardfan">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/bonus-cards/bonus-card1.webp`}
+          alt="bonus card one"
+        />
+        <img
+          src={`${process.env.PUBLIC_URL}/images/bonus-cards/bonus-card2.webp`}
+          alt="bonus card one"
+        />
+        <img
+          src={`${process.env.PUBLIC_URL}/images/bonus-cards/bonus-card3.webp`}
+          alt="bonus card one"
+        />
+      </div>
+
+      <button className='btn'>Learn more</button>
+    </section>
+  );
+};
+
+export default ActionsSection;
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import TeaNavigation from '../../routes/tea-navigation/tea-navigation.component';
 import CarouselContainer from '../../components/carousel-container/carousel-container.component';
 import SlideShowHover from '../../components/slide-show-hover/slide-show-hover.component';
-
+import ActionsSection from '../../components/actions-section/actions-section.component';
 
 import './home.styles.scss';
 
@@ -12,6 +12,7 @@ const Home = () => {
     <main>
       <CarouselContainer />
       <TeaNavigation />
+      <ActionsSection />
       <SlideShowHover />
 
       <Outlet />
