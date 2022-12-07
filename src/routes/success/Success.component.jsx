07c40b983a15ca5cd7c53f1adbaa62f94fe53c5b
OxanaSF <oxanatango@gmail.com>
2022-12-07 15:46:39 -0800

    style success page

--- src/routes/success/Success.component.jsx
+++ src/routes/success/Success.component.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import './success.styles.styles.scss';
 
 const Success = () => {
   return (
-    <h1>Thank you for your purchase!</h1>
-  )
-}
+    <section className='success-container'>
+      <img src={`${process.env.PUBLIC_URL}/images/gift-box-wood.jpg`} alt="gift box on woods" />
+      <h1>Thank you for your purchase!</h1>
+    </section>
+  );
+};
 
-export default Success
+export default Success;
